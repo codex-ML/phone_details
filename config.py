36c611fa82e73279632963e551349146a94b5d42
ANISH <153320703+codex-ML@users.noreply.github.com>
2024-06-25 20:04:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 api_id = 29268693
 api_hash = "1ad7c3f7c78b8ca2a1888b757764ae03"
-bot_token = "6764349911:AAGbdkcXZbzERauGsYXJuwK15RSz3RhECdQ"
+bot_token = "6764349911:AAGbdbzERauGsYXJuwK15RSz3RhECdQ"
